0be23d838223a84b498fe4a64ee671d21358f923
Bernardo Villalba <bdvillalba@teco.com.ar>
2024-09-05 10:11:49 -0300

    firt commit

---
+++ suma.py
@@ -0,0 +1,8 @@
+# Solicitar un número al usuario
+numero = int(input("Ingresa un número: "))
+
+# Sumar el número consigo mismo
+resultado = numero + numero
+
+# Imprimir el resultado
+print("El resultado es:", resultado)
